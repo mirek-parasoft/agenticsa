77375f4e824bcfd422af7bcce0276ec8d0e2f71a
mirek-parasoft <mirek@parasoft.com>
2026-04-29 10:14:40 +0200

    new feature implemented

--- goo.cpp
+++ goo.cpp
@@ -23,8 +23,6 @@ void setMotorSpeed(uint16_t speed)
 
 int computeControl(int sensor, int divisor)
 {
-    int value = 0;
-
     if (sensor > SENSOR_HIGH_THRESHOLD) {
         return sensor / divisor;
     } else if (sensor > SENSOR_LOW_THRESHOLD) {
@@ -32,13 +30,12 @@ int computeControl(int sensor, int divisor)
     } else {
         return DEFAULT_VALUE;
     }
-    return value;
 }
 
 int processCommand(const char* cmd, const char* arg)
 {
     if (strcmp(cmd, "SET") == 0) {
-        int val = atoi(arg);
+        const int val = atoi(arg);
         setMotorSpeed((uint16_t)val);
         return val;
     }
@@ -49,9 +46,10 @@ int processCommand(const char* cmd, const char* arg)
     return -1; 
 }
 
+//new code
 int processor(char* cmd, char* arg)
 {
-    uint16_t sensor = readSensor();
-    int control = computeControl(sensor, 0); 
+    const uint16_t sensor = readSensor();
+    const int control = computeControl(sensor, 0); 
     return processCommand(cmd, arg) + control;
 }
